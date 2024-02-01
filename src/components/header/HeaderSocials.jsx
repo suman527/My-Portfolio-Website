a32f55b5e6e727ce38eda0d91d441144526879ae
suman527 <sahusuman0133@gmail.com>
2024-02-01 23:52:12 +0530

    portfolio created

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BsLinkedin } from 'react-icons/bs';
+import { FaGithub } from 'react-icons/fa';
+
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a href="https://www.linkedin.com/in/suman-sahu5/" target="_blank" rel="noreferrer" ><BsLinkedin /></a>
+      <a href="https://github.com/suman527" target="_blank" rel="noreferrer" ><FaGithub /></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
